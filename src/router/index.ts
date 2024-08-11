a7724569a88d8d9c03d4ee406d6b92735f93fbc8
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-08-10 21:58:17 -0300

    refactor: Add routes for Stripe webhook and Todo
    This commit adds routes for the Stripe webhook and Todo in the router file. The Stripe webhook route is added to handle incoming webhook requests from Stripe, while the Todo route is added to handle todo-related requests. This improves code organization and separates the logic for handling different types of requests.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import { checkoutRouter } from "./checkout-router";
+import { stripeWebHookRouter } from "./Stripe-webhook-router";
+import { todoRouter } from "./Todo-router";
+import { userRouter } from "./User-router";
 
 export const router = Router();
 router.use("checkout", checkoutRouter);
+router.use("stripe-webhook", stripeWebHookRouter);
+router.use("todo", todoRouter);
+router.use("user", userRouter);
